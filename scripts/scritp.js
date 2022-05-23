9c347492a7e8ad272e7e0959423d234543410d1c
arthurpxx <fdtuca@gmail.com>
2022-05-23 16:29:26 -0300

    Jogo da memoria finalizado

--- scripts/scritp.js
+++ scripts/scritp.js
@@ -7,46 +7,71 @@ const ICON = "icon"
 startGame()
 
 function startGame() {
-    initializeCards(game.createCardsFromTechs())
+  initializeCards(game.createCardsFromTechs())
 }
 
 function initializeCards(cards) {
-    let gameBoard = document.getElementById("gameBoard")
-
-    cards.forEach(card => {
-        let cardElement = document.createElement('div')
-        cardElement.id = card.id
-        cardElement.classList.add(CARD)
-        cardElement.dataset.icon = card.icon
-        createCardContent(card, cardElement)
-
-        cardElement.addEventListener('click', flipCard)
-
-        gameBoard.appendChild(cardElement)
-    })
+  let gameBoard = document.getElementById("gameBoard")
+  gameBoard.innerHTML = ''
+  game.cards.forEach(card => {
+    let cardElement = document.createElement('div')
+    cardElement.id = card.id
+    cardElement.classList.add(CARD)
+    cardElement.dataset.icon = card.icon
+    createCardContent(card, cardElement)
+    cardElement.addEventListener('click', flipCard)
+    gameBoard.appendChild(cardElement)
+  })
 }
 
 function createCardContent(card, cardElement) {
-    createCardFace(FRONT, card, cardElement)
-    createCardFace(BACK, card, cardElement)
+  createCardFace(FRONT, card, cardElement)
+  createCardFace(BACK, card, cardElement)
 }
 
 function createCardFace(face, card, element) {
-    let cardElementFace = document.createElement('div')
-    cardElementFace.classList.add(face)
+  let cardElementFace = document.createElement('div')
+  cardElementFace.classList.add(face)
 
-    if (face === FRONT) {
-        let iconElement = document.createElement('img')
-        iconElement.classList.add(ICON)
-        iconElement.src = "/assets/" + card.icon + ".png"
-        cardElementFace.appendChild(iconElement)
-    } else {
-        cardElementFace.innerHTML = "&lt/&gt"
-    }
-    element.appendChild(cardElementFace)
+  if (face === FRONT) {
+    let iconElement = document.createElement('img')
+    iconElement.classList.add(ICON)
+    iconElement.src = "/assets/" + card.icon + ".png"
+    cardElementFace.appendChild(iconElement)
+  } else {
+    cardElementFace.innerHTML = "&lt/&gt"
+  }
+  element.appendChild(cardElementFace)
 }
 
 
 function flipCard() {
+  if (game.setCard(this.id)) {
     this.classList.add("flip")
+    if (game.secondCard) {
+      if (game.checkMath()) {
+        game.clearCards()
+        if(game.checkGameOver()){
+          let gameOverLayer = document.getElementById('gameOver')
+          gameOverLayer.style.display = 'flex'
+        }
+      } else {
+        setTimeout(() => {
+          let firstCardView = document.getElementById(game.firstCard.id)
+          let secondCardView = document.getElementById(game.secondCard.id)
+
+          firstCardView.classList.remove('flip')
+          secondCardView.classList.remove('flip')
+          game.unflipCards() 
+        }, 500)
+      }
+    }
+  }
+}
+
+function restart(){
+  game.clearCards()
+  startGame()
+  let gameOverLayer = document.getElementById('gameOver')
+  gameOverLayer.style.display = 'none'
 }
